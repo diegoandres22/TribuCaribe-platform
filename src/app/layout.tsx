ec8b74cf604c6fb4ebcf6c129eb57fe695d8df66
diegoandres22 <diego.a.v3005@gmail.com>
2024-09-12 20:08:49 -0400

    Primer Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { NextUIProvider } from '@nextui-org/react';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NextUIProvider>
+          {children}
+        </NextUIProvider>
       </body>
     </html>
   );
