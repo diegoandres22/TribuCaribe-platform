e5a1ba46c5e551cb367e32518fc8b71b9c4f4a24
Adams contreras <adamscontreras18@gmail.com>
2024-09-23 16:58:12 -0300

    Feat button props

---
+++ src/components/elements/button.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+
+interface ButtonProps {
+  value: string;
+  bg: string;
+  color: string;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ value, bg, color, onClick }) => {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        className={`p-3 ${bg} rounded-xl ${color} shadow-lg hover:scale-95 transition-all`}
+      >
+        {value}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
