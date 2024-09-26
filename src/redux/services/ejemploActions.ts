60ef66f5d5d5bf766bfdcb269d73867c71459392
diegoandres22 <diego.a.v3005@gmail.com>
2024-09-25 20:13:56 -0400

    FIX [Redux]: corrijo producción de la cagada que hizo robert

--- src/redux/services/ejemploActions.ts
+++ src/redux/services/ejemploActions.ts
@@ -1,24 +1,24 @@
-import axios, { AxiosError } from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 interface respuestaDelBack {
-    name: string,
-    id: string
+  name: string;
+  id: string;
 }
 
 const url = `url del back que debe estar en el archivo .env`;
 
-export const getEjemplo = createAsyncThunk<respuestaDelBack, string, { rejectValue: string }>(
-    'nameDeSlice/getEjemplo',
-    async (idEjemplo, { rejectWithValue }) => {
-        try {
-            const response = await axios.get<any>(`${url}/${idEjemplo}`);
-            const data = response.data;
-            return data;
-
-        } catch (error: AxiosError | any) {
-            console.error(error.response.data);
-            return rejectWithValue("string mensaje de error");
-        }
-    }
-);
+export const getEjemplo = createAsyncThunk<
+  respuestaDelBack,
+  string,
+  { rejectValue: string }
+>("nameDeSlice/getEjemplo", async (idEjemplo, { rejectWithValue }) => {
+  try {
+    const response = await axios.get<any>(`${url}/${idEjemplo}`);
+    const data = response.data;
+    return data;
+  } catch (error: any) {
+    console.error(error.response.data);
+    return rejectWithValue("string mensaje de error");
+  }
+});
