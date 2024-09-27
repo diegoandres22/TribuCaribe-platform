d6a442b89d66ae88fc924fdf1c5156e3ed73bbce
Adams contreras <adamscontreras18@gmail.com>
2024-09-27 16:58:12 -0300

    Fix responsive AdvertisingCarousel & Global state DeviceDetector with Redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { NextUIProvider } from "@nextui-org/react";
 import { Providers } from "@/redux/providers";
+import Head from "next/head";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import DeviceDetector from "@/components/logicsFuntions/DeviceDetectorActions";
 
 export const metadata: Metadata = {
   title: "Tribu Caribe",
@@ -17,8 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <Head>
+        {/* Meta etiqueta para evitar el zoom en dispositivos móviles */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
       <body>
         <Providers>
+          <DeviceDetector></DeviceDetector>
           <NextUIProvider>{children}</NextUIProvider>
         </Providers>
       </body>
