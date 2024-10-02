259a1be951388486ae688c49ab2609199019b13f
unknown <robertoviedo35@gmail.com>
2024-10-02 14:00:49 -0400

    Fix[Footer]: nuevo diseño

--- public/hotCategories/icons.tsx
+++ public/hotCategories/icons.tsx
@@ -3,6 +3,12 @@ import { MdOutlineCookie } from "react-icons/md";
 import { FaWineBottle } from "react-icons/fa";
 import { MdLocalDrink } from "react-icons/md";
 import { FaCandyCane } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 export const Cookie = () => {
   return (
@@ -32,6 +38,54 @@ export const Candy = () => {
   return (
     <div>
       <FaCandyCane />
+    </div>
+  );
+};
+
+export const Facebook = () => {
+  return (
+    <div>
+      <FaFacebookSquare />
+    </div>
+  );
+};
+
+export const Linkedin = () => {
+  return (
+    <div>
+      <FaLinkedinIn />
+    </div>
+  );
+};
+
+export const Twitter = () => {
+  return (
+    <div>
+      <FaXTwitter />
+    </div>
+  );
+};
+
+export const Instagram = () => {
+  return (
+    <div>
+      <FaInstagram />
+    </div>
+  );
+};
+
+export const Youtube = () => {
+  return (
+    <div>
+      <FaYoutube />
+    </div>
+  );
+};
+
+export const Tiktok = () => {
+  return (
+    <div>
+      <FaTiktok />
     </div>
   );
 };
