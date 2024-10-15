b3ab319c975ea982ea024a0ab251a5c3d664c705
RobertAntonioOviedoSanchez <robertoviedo35@gmail.com>
2024-10-14 23:17:36 -0400

    Fix[profile errors]

---
+++ src/redux/slices/profileSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getProfile } from "../services/profileActions";
+import { ProfileProps } from "@/types";
+
+interface ProfileState {
+  profile: ProfileProps | null;
+  loading: boolean;
+  error: string | null | undefined;
+}
+
+const initialState: ProfileState = {
+  profile: null,
+  loading: false,
+  error: null,
+};
+
+const profileSlice = createSlice({
+  name: "profile",
+  initialState,
+  reducers: {
+    funcionEjemploo: (state) => {
+      state.loading = false
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProfile.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProfile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.profile = action.payload;
+      })
+      .addCase(getProfile.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { funcionEjemploo } = profileSlice.actions;
+
+export default profileSlice.reducer;
