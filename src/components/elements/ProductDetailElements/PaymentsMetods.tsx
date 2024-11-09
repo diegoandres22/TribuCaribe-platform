3e80c059d97d9c05baf8e421c3c5693898037050
diegoandres22 <diego.a.v3005@gmail.com>
2024-11-08 20:47:39 -0400

    FIX[paymentsMetods, price]: coloco boton al icono, para que funcione y no recargue la pagina

--- src/components/elements/ProductDetailElements/PaymentsMetods.tsx
+++ src/components/elements/ProductDetailElements/PaymentsMetods.tsx
@@ -1,5 +1,6 @@
 import {
   Image,
+  Link,
   Popover,
   PopoverContent,
   PopoverTrigger,
@@ -15,7 +16,9 @@ export const PaymentsMetods = () => {
         <h3 className="font-bold ">Métodos de pago</h3>
         <Popover placement="right">
           <PopoverTrigger>
-            <CiCircleQuestion className="mx-2 text-xl text-blue-600" />
+            <button type="button" className="mx-2 text-xl text-blue-600 focus:outline-none"            >
+              <CiCircleQuestion />
+            </button>
           </PopoverTrigger>
           <PopoverContent>
             <div className="px-2 py-2">
--- src/components/elements/ProductDetailElements/Price.tsx
+++ src/components/elements/ProductDetailElements/Price.tsx
@@ -1,6 +1,8 @@
-import { Popover, PopoverContent, PopoverTrigger } from "@nextui-org/react";
+
 import React from "react";
+import Link from "next/link";
 import { CiCircleQuestion } from "react-icons/ci";
+import { Popover, PopoverTrigger, PopoverContent } from "@nextui-org/react";
 
 interface PriceProps {
   priceInDollars: number;
@@ -12,17 +14,24 @@ export const Price: React.FC<PriceProps> = ({ priceInDollars }) => {
 
   return (
     <div className="p-4 flex flex-col ">
+
       <div className="text-2xl font-bold flex">
         US$ {priceInDollars}
         <sup className="text-xs top-1 ml-1">80</sup>
         <Popover placement="right">
           <PopoverTrigger>
-            <CiCircleQuestion className="mx-2 text-xl text-blue-600" />
+            <button
+              type="button"
+              className="mx-2 text-xl text-blue-600 focus:outline-none"
+            >
+              <CiCircleQuestion />
+            </button>
           </PopoverTrigger>
-          <PopoverContent>
-            <div className="px-2 py-2">
-              <div className="text-small font-bold">Tasa de cambio</div>
-              <div className="text-tiny">
+          <PopoverContent >
+            <div className='p-2'>
+
+              <div className="text-small font-bold left-0">Tasa de cambio</div>
+              <div className="text-xs">
                 La tasa de cambio utilizada es de <strong>36 Bs.</strong> por
                 cada dólar.
                 <br />
@@ -32,6 +41,7 @@ export const Price: React.FC<PriceProps> = ({ priceInDollars }) => {
           </PopoverContent>
         </Popover>
       </div>
+
       <div className="text-sm text-gray-500">
         Bs. {priceInBolivars.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
       </div>
